3b14b8b2f43274890952faa6602f85d9c7457c26
tdbts <VinnySanchez87@gmail.com>
2015-04-30 12:13:33 -0400

    Altered routes to accomodate for dynamic addition of .active class to navbar menu.

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 router.get('*', function (req, res, next) {
-	req.activeRoute = req.activeRoute || '/';
+	req.activeRoute = req.activeRoute || '/'; 
+
+	req.viewOptions = {
+		title: "tdbts | A Humble Page for Humble Projects", 
+		activeTab: req.activeRoute
+	};
 
 	next();
 });
 
 router.get('/', function(req, res) {
 
-	res.render('navigation', { 
-		title: "tdbts | A Humble Page for Humble Projects", 
-		activeTab: req.activeRoute 
-	});
+	res.render('navigation', req.viewOptions);
+});
+
+router.get('/:path', function (req, res) {
+
+	req.viewOptions.activeTab = req.path; 
+	
+	res.render('navigation', req.viewOptions);
 });
 
 module.exports = router;
