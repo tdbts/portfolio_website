af97e10c3217d54c7e69990a8a3c8ce6da4e6c8a
tdbts <VinnySanchez87@gmail.com>
2015-05-02 18:40:55 -0400

    Removed jQuery to add class offPageLeft/Right to CareerDescription component.  Start of a process of removing all calls to jQuery.addClass() as it's not the 'React Way'

--- public/components/CareerDescription.jsx
+++ public/components/CareerDescription.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 
 var CareerDescription = React.createClass({
+	
 	getStyles: function () {
 		return {
 			careerDescriptionContainer: {
@@ -11,7 +12,7 @@ var CareerDescription = React.createClass({
 				fontSize: "1.75em",
 				letterSpacing: "3px",  
 				color: "#e1e1e1", 
-				textShadow: "2px 2px 10px #2b2b2b", 
+				textShadow: "8px 7px 10px #2b2b2b", 
 				marginTop: "10px"
 			},
 
@@ -26,16 +27,11 @@ var CareerDescription = React.createClass({
 		return React.findDOMNode(this.refs[ref]);
 	},	
 
-	componentDidMount: function () {
-		$(this.getRefDOMNode('descriptionLeft')).addClass('offPageLeft');
-		$(this.getRefDOMNode('descriptionRight')).addClass('offPageRight');	
-	}, 
-
 	render: function () {
 		return (
 			<div id="career_description_container" style={this.getStyles().careerDescriptionContainer}>
 				<h1 id="careerDescription" className="lead" style={this.getStyles().careerDescription}>
-					<span id="descriptionLeft" ref="descriptionLeft" style={this.getStyles().unbreakable}>Web Development /</span> <span id="descriptionRight" ref="descriptionRight" style={this.getStyles().unbreakable}>Data Makeover Artist</span>
+					<span id="descriptionLeft" className="offPageLeft" ref="descriptionLeft" style={this.getStyles().unbreakable}>Web Development /</span> <span id="descriptionRight" className="offPageRight" ref="descriptionRight" style={this.getStyles().unbreakable}>Data Makeover Artist</span>
 				</h1>
 			</div>			
 		);
--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -19689,6 +19689,7 @@ module.exports = require('./lib/React');
 var React = require('react');
 
 var CareerDescription = React.createClass({displayName: "CareerDescription",
+	
 	getStyles: function () {
 		return {
 			careerDescriptionContainer: {
@@ -19699,7 +19700,7 @@ var CareerDescription = React.createClass({displayName: "CareerDescription",
 				fontSize: "1.75em",
 				letterSpacing: "3px",  
 				color: "#e1e1e1", 
-				textShadow: "2px 2px 10px #2b2b2b", 
+				textShadow: "8px 7px 10px #2b2b2b", 
 				marginTop: "10px"
 			},
 
@@ -19714,16 +19715,11 @@ var CareerDescription = React.createClass({displayName: "CareerDescription",
 		return React.findDOMNode(this.refs[ref]);
 	},	
 
-	componentDidMount: function () {
-		$(this.getRefDOMNode('descriptionLeft')).addClass('offPageLeft');
-		$(this.getRefDOMNode('descriptionRight')).addClass('offPageRight');	
-	}, 
-
 	render: function () {
 		return (
 			React.createElement("div", {id: "career_description_container", style: this.getStyles().careerDescriptionContainer}, 
 				React.createElement("h1", {id: "careerDescription", className: "lead", style: this.getStyles().careerDescription}, 
-					React.createElement("span", {id: "descriptionLeft", ref: "descriptionLeft", style: this.getStyles().unbreakable}, "Web Development /"), " ", React.createElement("span", {id: "descriptionRight", ref: "descriptionRight", style: this.getStyles().unbreakable}, "Data Makeover Artist")
+					React.createElement("span", {id: "descriptionLeft", className: "offPageLeft", ref: "descriptionLeft", style: this.getStyles().unbreakable}, "Web Development /"), " ", React.createElement("span", {id: "descriptionRight", className: "offPageRight", ref: "descriptionRight", style: this.getStyles().unbreakable}, "Data Makeover Artist")
 				)
 			)			
 		);
