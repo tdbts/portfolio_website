ca2e59c3361699e18467a576c8eead8fea20c0ef
tdbts <VinnySanchez87@gmail.com>
2015-06-09 17:41:07 -0400

    Rebundle.

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -29309,7 +29309,7 @@ var AcknowledgementsScrollable = React.createClass({displayName: "Acknowledgemen
 
 module.exports = AcknowledgementsScrollable;
 
-},{"./ScrollableSection":173,"react":157}],164:[function(require,module,exports){
+},{"./ScrollableSection":172,"react":157}],164:[function(require,module,exports){
 var React = require('react');
 
 var CareerDescription = React.createClass({displayName: "CareerDescription",
@@ -29523,149 +29523,7 @@ var ContactPage = React.createClass({displayName: "ContactPage",
 
 module.exports = ContactPage;
 
-},{"./ContactCircle":165,"classnames":187,"react":157}],167:[function(require,module,exports){
-var React = require('react'), 
-	invokeAll = require('../javascripts/invokeAll'), 
-	$ = window.jQuery || require('jquery');
-
-var EmailModal = React.createClass({displayName: "EmailModal",
-	getVal: function (selector) {
-		
-		return $(selector).val();
-	}, 
-
-	hideModal: function () {
-
-		$('#emailModal').modal('hide');
-	}, 
-
-	clearField: function (selectors) {
-		
-		selectors.forEach(function (fieldID) {
-			
-			$(fieldID).val("");
-		});
-	}, 
-
-	activatePopover: function(popoverID) {
-
-		$(popoverID).popover({content: 'Thanks for reaching out!'}, 'click');
-	}, 
-
-	hidePopover: function () {
-		
-		$('#send_email_btn').popover('hide');
-	}, 
-
-	activateEmailModal: function () {
-		// var getVal = this.getVal, 
-		// 	hideModal = this.hideModal, 
-		// 	hidePopover = this.hidePopover, 
-		// 	clearField
-
-		$('#send_email_btn').on('click', function(event) {
-
-			var firstName = this.getVal('#first_name'),
-				lastName = this.getVal('#last_name'),
-				email = this.getVal('#email'),
-				comments = this.getVal('#comments'),
-				url = '/sendEmail';
-
-			var request = $.ajax({
-
-				type: "POST",
-				url: url,
-				data: { 
-					firstName: firstName,
-					lastName: lastName,
-					email: email,
-					comments: comments
-				} 
-			});
-
-			request.done(function() {
-
-				invokeAll([
-					this.hideModal, 
-					function () {
-						this.clearField(['#first_name', '#last_name', '#email', '#comments']);
-					}, 
-					this.hidePopover
-				]);
-			}.bind(this));
-
-			request.fail(function() {
-				
-				alert('Sorry, AJAX was unable to process that request!');
-			});
-
-			event.preventDefault();
-		}.bind(this));		
-	}, 
-
-	componentDidMount: function () {
-		
-		invokeAll([this.activatePopover, this.activateEmailModal])
-	}, 
-
-	render: function () {
-		var emailInputStyle = {
-				paddingLeft: "15px", 
-				paddingRight: "15px"
-			};
-
-		return (
-			React.createElement("div", {id: "emailModal", className: "modal fade", tabIndex: "-1", role: "dialog", "aria-labelledby": "myModalLabel", "aria-hidden": "true"}, 
-				React.createElement("div", {className: "modal-dialog"}, 
-					React.createElement("div", {className: "modal-content"}, 
-						React.createElement("div", {className: "modal-header"}, 
-							React.createElement("button", {type: "button", className: "close", "data-dismiss": "modal", "aria-hidden": "true"}, "×"), 
-							React.createElement("h4", {className: "modal-title"}, "Contact Form")
-						), 
-						React.createElement("div", {className: "modal-body"}, 
-							React.createElement("form", {action: "#", name: "commentform", method: "post", id: "emailForm", className: "form-horizontal"}, 
-								React.createElement("div", {className: "form-group"}, 
-									React.createElement("label", {htmlFor: "first_name", className: "control-label col-md-4"}, "First Name"), 
-									React.createElement("div", {className: "col-md-6"}, 
-										React.createElement("input", {type: "text", className: "form-control", id: "first_name", name: "first_name", placeholder: "First Name"})
-									)
-								), 
-								React.createElement("div", {className: "form-group"}, 
-									React.createElement("label", {htmlFor: "last_name", className: "control-label col-md-4"}, "Last Name"), 
-									React.createElement("div", {className: "col-md-6"}, 
-										React.createElement("input", {type: "text", className: "form-control", id: "last_name", name: "last_name", placeholder: "Last Name"})
-									)
-								), 
-								React.createElement("div", {className: "form-group"}, 
-									React.createElement("label", {htmlFor: "email", className: "control-label col-md-4"}, "Email Address"), 
-									React.createElement("div", {className: "col-md-6 input-group", style: emailInputStyle}, 
-										React.createElement("span", {className: "input-group-addon"}, "@"), 
-										React.createElement("input", {type: "email", className: "form-control", id: "email", name: "email", placeholder: "Email Address"})
-									)
-								), 
-								React.createElement("div", {className: "form-group"}, 
-									React.createElement("label", {htmlFor: "comment", className: "control-label col-md-4"}, "Question or Comment"), 
-									React.createElement("div", {className: "col-md-6"}, 
-										React.createElement("textarea", {name: "comments", id: "comments", rows: "6", className: "form-control", placeholder: "Your question or comment here"})
-									)
-								), 
-								React.createElement("div", {className: "form-group"}, 
-									React.createElement("div", {className: "col-md-6"}, 
-										React.createElement("button", {className: "btn btn-primary pull-right", type: "button", value: "Submit", id: "send_email_btn"}, "Send")
-									)
-								)
-							)
-						)
-					)
-				)
-			)			
-		);
-	}
-});
-
-module.exports = EmailModal;
-
-},{"../javascripts/invokeAll":186,"jquery":2,"react":157}],168:[function(require,module,exports){
+},{"./ContactCircle":165,"classnames":185,"react":157}],167:[function(require,module,exports){
 var React = require('react');
 
 var FeedCloudOptions = React.createClass({displayName: "FeedCloudOptions",
@@ -29682,7 +29540,7 @@ var FeedCloudOptions = React.createClass({displayName: "FeedCloudOptions",
 
 module.exports = FeedCloudOptions;
 
-},{"react":157}],169:[function(require,module,exports){
+},{"react":157}],168:[function(require,module,exports){
 var React = require('react'), 
 	LandingPageElements = require('./LandingPageElements');
 
@@ -29707,7 +29565,7 @@ var LandingPage = React.createClass({displayName: "LandingPage",
 
 module.exports = LandingPage;
 
-},{"./LandingPageElements":170,"react":157}],170:[function(require,module,exports){
+},{"./LandingPageElements":169,"react":157}],169:[function(require,module,exports){
 var React = require('react'), 
 	Name = require('./Name'), 
 	CareerDescription = require('./CareerDescription'), 
@@ -29791,7 +29649,7 @@ var LandingPageHeader = React.createClass({displayName: "LandingPageHeader",
 
 module.exports = LandingPageHeader;
 
-},{"./CareerDescription":164,"./Name":171,"./WelcomeButton":184,"react":157}],171:[function(require,module,exports){
+},{"./CareerDescription":164,"./Name":170,"./WelcomeButton":183,"react":157}],170:[function(require,module,exports){
 var React = require('react');
 
 var Name = React.createClass({displayName: "Name",
@@ -29824,13 +29682,12 @@ var Name = React.createClass({displayName: "Name",
 
 module.exports = Name;
 
-},{"react":157}],172:[function(require,module,exports){
+},{"react":157}],171:[function(require,module,exports){
 var React = require('react'), 
 	LandingPage = require('./LandingPage'), 
 	AcknowledgementsPage = require('./AcknowledgementsPage'), 
 	ContactPage = require('./ContactPage'), 
-	TweetCloud = require('./TweetCloud'), 
-	EmailModal = require('./EmailModal');
+	TweetCloud = require('./TweetCloud');
 
 var PortfolioPageContent = React.createClass({displayName: "PortfolioPageContent",
 	determineViewToRender: function () {
@@ -29867,8 +29724,7 @@ var PortfolioPageContent = React.createClass({displayName: "PortfolioPageContent
 	render: function () {
 		return (
 			React.createElement("div", {id: "#page_component_anchor", className: "fullHeightWidth"}, 
-				this.determineViewToRender(), 
-				React.createElement(EmailModal, null)
+				this.determineViewToRender()
 			)
 		);
 	}
@@ -29876,7 +29732,7 @@ var PortfolioPageContent = React.createClass({displayName: "PortfolioPageContent
 
 module.exports = PortfolioPageContent;
 
-},{"./AcknowledgementsPage":162,"./ContactPage":166,"./EmailModal":167,"./LandingPage":169,"./TweetCloud":176,"react":157}],173:[function(require,module,exports){
+},{"./AcknowledgementsPage":162,"./ContactPage":166,"./LandingPage":168,"./TweetCloud":175,"react":157}],172:[function(require,module,exports){
 var React = require('react'), 
 	ScrollableSectionContent = require('./ScrollableSectionContent');
 
@@ -29892,7 +29748,7 @@ var ScrollableSection = React.createClass({displayName: "ScrollableSection",
 
 module.exports = ScrollableSection;
 
-},{"./ScrollableSectionContent":174,"react":157}],174:[function(require,module,exports){
+},{"./ScrollableSectionContent":173,"react":157}],173:[function(require,module,exports){
 var React = require('react');
 
 var ScrollableSectionContent = React.createClass({displayName: "ScrollableSectionContent",
@@ -29912,7 +29768,7 @@ var ScrollableSectionContent = React.createClass({displayName: "ScrollableSectio
 
 module.exports = ScrollableSectionContent;
 
-},{"react":157}],175:[function(require,module,exports){
+},{"react":157}],174:[function(require,module,exports){
 var React = require('react');
 
 var SearchCloudOptions = React.createClass({displayName: "SearchCloudOptions",
@@ -29930,7 +29786,7 @@ var SearchCloudOptions = React.createClass({displayName: "SearchCloudOptions",
 
 module.exports = SearchCloudOptions;
 
-},{"react":157}],176:[function(require,module,exports){
+},{"react":157}],175:[function(require,module,exports){
 var React = require('react'), 
 	TweetCloudMenu = require('./TweetCloudMenu'), 
 	TweetCloudSearchInput = require('./TweetCloudSearchInput'), 
@@ -29975,7 +29831,7 @@ var TweetCloud = React.createClass({displayName: "TweetCloud",
 
 module.exports = TweetCloud;
 
-},{"./TweetCloudImageModal":177,"./TweetCloudMenu":178,"./TweetCloudSearchInput":183,"jquery":2,"react":157}],177:[function(require,module,exports){
+},{"./TweetCloudImageModal":176,"./TweetCloudMenu":177,"./TweetCloudSearchInput":182,"jquery":2,"react":157}],176:[function(require,module,exports){
 var React = require('react');
 
 var TweetCloudImageModal = React.createClass({displayName: "TweetCloudImageModal",
@@ -30005,7 +29861,7 @@ var TweetCloudImageModal = React.createClass({displayName: "TweetCloudImageModal
 
 module.exports = TweetCloudImageModal;
 
-},{"react":157}],178:[function(require,module,exports){
+},{"react":157}],177:[function(require,module,exports){
 var React = require('react'), 
 	AccordionPanel = require('./AccordionPanel'), 
 	TweetCloudMenuBodyWhatItIs = require('./TweetCloudMenuBodyWhatItIs'), 
@@ -30046,7 +29902,7 @@ var TweetCloudMenu = React.createClass({displayName: "TweetCloudMenu",
 
 module.exports = TweetCloudMenu;
 
-},{"./AccordionPanel":158,"./TweetCloudMenuBodyHowItWorks":179,"./TweetCloudMenuBodySearchOptions":180,"./TweetCloudMenuBodyWhatItIs":181,"react":157}],179:[function(require,module,exports){
+},{"./AccordionPanel":158,"./TweetCloudMenuBodyHowItWorks":178,"./TweetCloudMenuBodySearchOptions":179,"./TweetCloudMenuBodyWhatItIs":180,"react":157}],178:[function(require,module,exports){
 var React = require('react');
 
 var TweetCloudMenuBodyHowItWorks = React.createClass({displayName: "TweetCloudMenuBodyHowItWorks",
@@ -30066,7 +29922,7 @@ var TweetCloudMenuBodyHowItWorks = React.createClass({displayName: "TweetCloudMe
 
 module.exports = TweetCloudMenuBodyHowItWorks;
 
-},{"react":157}],180:[function(require,module,exports){
+},{"react":157}],179:[function(require,module,exports){
 var React = require('react'), 
 	TweetCloudOptions = require('./TweetCloudOptions');
 
@@ -30102,7 +29958,7 @@ var TweetCloudMenuBodySearchOptions = React.createClass({displayName: "TweetClou
 
 module.exports = TweetCloudMenuBodySearchOptions;
 
-},{"./TweetCloudOptions":182,"react":157}],181:[function(require,module,exports){
+},{"./TweetCloudOptions":181,"react":157}],180:[function(require,module,exports){
 var React = require('react');
 
 var TweetCloudMenuBodyWhatItIs = React.createClass({displayName: "TweetCloudMenuBodyWhatItIs",
@@ -30117,7 +29973,7 @@ var TweetCloudMenuBodyWhatItIs = React.createClass({displayName: "TweetCloudMenu
 
 module.exports = TweetCloudMenuBodyWhatItIs;
 
-},{"react":157}],182:[function(require,module,exports){
+},{"react":157}],181:[function(require,module,exports){
 var React = require('react'), 
 	FeedCloudOptions = require('./FeedCloudOptions'), 
 	SearchCloudOptions = require('./SearchCloudOptions');
@@ -30135,7 +29991,7 @@ var TweetCloudOptions = React.createClass({displayName: "TweetCloudOptions",
 
 module.exports = TweetCloudOptions;
 
-},{"./FeedCloudOptions":168,"./SearchCloudOptions":175,"react":157}],183:[function(require,module,exports){
+},{"./FeedCloudOptions":167,"./SearchCloudOptions":174,"react":157}],182:[function(require,module,exports){
 var React = require('react');
 
 var TweetCloudSearchInput = React.createClass({displayName: "TweetCloudSearchInput",
@@ -30155,7 +30011,7 @@ var TweetCloudSearchInput = React.createClass({displayName: "TweetCloudSearchInp
 
 module.exports = TweetCloudSearchInput;
 
-},{"react":157}],184:[function(require,module,exports){
+},{"react":157}],183:[function(require,module,exports){
 var React = require('react');
 
 var WelcomeButton = React.createClass({displayName: "WelcomeButton",
@@ -30236,7 +30092,7 @@ var WelcomeButton = React.createClass({displayName: "WelcomeButton",
 
 module.exports = WelcomeButton;
 
-},{"react":157}],185:[function(require,module,exports){
+},{"react":157}],184:[function(require,module,exports){
 var React = require('react'),  
 	PortfolioPageContent = require('./PortfolioPageContent');
 
@@ -30245,17 +30101,7 @@ $(document).ready(function() {
 		React.render(React.createElement(PortfolioPageContent, null), document.getElementById('page_content_wrapper'));
 });
 
-},{"./PortfolioPageContent":172,"react":157}],186:[function(require,module,exports){
-module.exports = function (arrayOfFunctions) {
-	
-	arrayOfFunctions.forEach(function (func) {
-		
-		func();
-	});
-	
-};
-
-},{}],187:[function(require,module,exports){
+},{"./PortfolioPageContent":171,"react":157}],185:[function(require,module,exports){
 /*!
   Copyright (c) 2015 Jed Watson.
   Licensed under the MIT License (MIT), see
@@ -30304,4 +30150,4 @@ if (typeof define !== 'undefined' && define.amd) {
 	});
 }
 
-},{}]},{},[185]);
+},{}]},{},[184]);
